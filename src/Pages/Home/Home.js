5932c3f70ea52034e674ff61a7b09a9c12f60d7a
Aninditaroy <anindita.roy.mu@gmail.com>
2022-06-15 01:00:49 +0600

    story section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Story from './Story/Story';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Story />
         </div>
     );
 };
