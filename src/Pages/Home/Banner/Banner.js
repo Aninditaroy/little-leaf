db2a75882d2ce57c8e698e6faf2e610b5a2c76ff
Sajeda Saju <sajedasaju85@gmail.com>
2022-06-14 22:31:43 +0600

    banner modified

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css";
+
 const Banner = () => {
     return (
-        <div>
+        <div className='mb-24'>
             <div
                 id="carouselDarkVariant"
                 className="carousel slide carousel-fade carousel-dark relative"
@@ -33,42 +32,103 @@ const Banner = () => {
                 {/* <!-- Inner --> */}
                 <div className="carousel-inner relative w-full overflow-hidden">
                     {/* <!-- Single item --> */}
-                    <div className="carousel-item active relative float-left w-full">
+                    {/* <div className="carousel-item active relative float-left w-full"> 
 
-                        <div className="sm:p-24 bg-[#d47752b0] grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3 lg:p-0" style={{ height: '750px' }}>
+                    <div className="sm:p-24 bg-[#d47752b0] grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3 lg:p-0" style={{ height: '750px' }}>
                             <img src='https://i.ibb.co/44Z0Gbj/slider-51-removebg-preview.png' alt="" className='lg:px-40 lg:w-full sm:w-80 lg:m-20 lg:p-0 md:p-0 sm:p-0' />
                             <div>
                                 <h1 className='lg:px-10 text-5xl lg:text-5xl lg:ml-11 lg:mt-80 font-bold' style={{ paddingLeft: '15px' }}>Give the gift of Greenery</h1>
                                 <h4 className='lg:px-48 text-xl text-slate-300 lg:ml-12 font-regular mt-5' style={{ paddingLeft: '15px' }}>Make your days feeling goods with beautiful plant.Whoever plants, gives God a reason to smile.</h4>
                             </div>
-                        </div>
-                    </div>
+                        </div> 
 
 
-                    {/* <!-- Single item --> */}
-                    <div className="carousel-item relative float-left w-full bottom-0 lg:bottom-20">
-                        <div className="sm:p-24 bg-[#F6EDEE] grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3 lg:p-0" style={{ height: '750px' }}>
-                            <img src='https://i.ibb.co/3NtNkcC/bunny-ears-cactus-flowerpot-53876-146249-removebg-preview.png' alt="" className='lg:px-40 lg:w-full sm:w-80 lg:m-20 lg:p-0 md:p-0 sm:p-0' />
-                            <div>
-                                <h1 className='lg:px-10 text-5xl lg:text-5xl  lg:ml-11 lg:mt-80 font-bold' style={{ paddingLeft: '15px' }}>Go the green way</h1>
-                                <h4 className='lg:px-48 text-xl  text-amber-800 lg:ml-12 font-regular mt-5' style={{ paddingLeft: '15px' }}>Let your house have a breath of fresh air.Mother earth is quite happy with your decision..</h4>
+
+                     </div> */}
+
+
+                    {/* original */}
+
+                    <div className="carousel-item active relative float-left w-full">
+
+                        <div className=" p-0 md:p-6  lg:p-24 xl:p-24 2xl:p-24 2xl:py-12  bg-[#c0795db0] " >
+                            <div className='w-11/12 md:w-full xl:w-11/12  2xl:w-11/12	 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2'>
+                                <img src='https://i.ibb.co/44Z0Gbj/slider-51-removebg-preview.png' alt="" className='' style={{ height: '436px', width: '436px' }} />
+                                <div className='flex items-center'>
+                                    <div className='lg:mt-24'>
+                                        <h1 className='lg:px-10 text-5xl md:text-4xl lg:text-5xl lg:ml-11  font-bold' style={{ paddingLeft: '15px' }}>Give the gift of Greenery</h1>
+                                        <h4 className='lg:pl-48 text-xl text-slate-300 lg:ml-12 font-regular mt-5 pr-0 mb-10' style={{ paddingLeft: '15px' }}>Make your days feeling goods with beautiful plant.Whoever plants, gives God a reason to smile.</h4>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
                     </div>
 
-                    {/* <!-- Single item --> */}
-                    <div className="carousel-item relative float-left w-full ">
+
+
+
+
+
+
+
+
+
+
+                    {/* second item for test */}
+
+                    <div className="carousel-item  relative float-left w-full  ">
+
+                        <div className=" p-0 md:p-6  lg:p-24 xl:p-24 2xl:p-24 2xl:py-12 bg-[#5B6963] " >
+                            <div className='w-11/12 md:w-full xl:w-11/12  2xl:w-11/12	 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 '>
+
+                                <img src='https://i.ibb.co/9qmf0g0/Zamioculcas-zamiifolia-ZZ-Plant-Charlie-Plant-Pot-Low-Stand-Grey-Washed-9287300c-e6ca-4a6d-abb0-4d8a.png' alt="" className='' style={{ height: '436px', width: '436px' }} />
+                                <div className='flex items-center'>
+                                    <div className='lg:mt-24'>
+                                        <h1 className='lg:px-10 text-5xl md:text-4xl lg:text-5xl lg:ml-11  font-bold' style={{ paddingLeft: '15px' }}>Go the green way</h1>
+                                        <h4 className='lg:pl-48 text-xl text-slate-300 lg:ml-12 font-regular mt-5 pr-0 mb-10' style={{ paddingLeft: '15px' }}>Let your house have a breath of fresh air.Mother earth is quite happy with your decision.</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                    {/* <!-- Single item 3 --> */}
+
+                    <div className="carousel-item  relative float-left w-full   ">
+
+                        <div className=" p-0 md:p-6  lg:p-24 xl:p-24 2xl:p-24 2xl:py-12 bg-[#B4C3AE] " >
+                            <div className='w-11/12 md:w-full xl:w-11/12  2xl:w-11/12	 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 '>
+
+                                {/* <img src='https://i.ibb.co/dGY6Pjc/alex-perri-m2-D-AFu-WV9s-unsplash-removebg-preview.png' alt="" className='' style={{ height: '436px', width: '436px' }} /> */}
+                                <img src=' https://i.ibb.co/k8PtkwZ/1623877401-medium-plant-spider-plant-seafoam-pot-2048x-removebg-preview.png' alt="" className='' style={{ height: '436px', width: '436px' }} />
+                                <div className='flex items-center'>
+                                    <div className='lg:mt-24'>
+                                        <h1 className='lg:px-10 text-5xl md:text-4xl lg:text-5xl lg:ml-11  font-bold' style={{ paddingLeft: '15px' }}>Go the green way</h1>
+                                        <h4 className='lg:pl-48 text-xl text-slate-300 lg:ml-12 font-regular mt-5 pr-0 mb-10' style={{ paddingLeft: '15px' }}>Let your house have a breath of fresh air.Mother earth is quite happy with your decision.</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                    {/* <div className="carousel-item relative float-left w-full ">
 
                         <div className="sm:p-24 relative bg-[#B4C3AE] grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3 lg:p-0 " style={{ height: '750px' }}>
                             <img src=' https://i.ibb.co/k8PtkwZ/1623877401-medium-plant-spider-plant-seafoam-pot-2048x-removebg-preview.png' alt="" className='relative bottom-0 lg:bottom-11 lg:px-40 lg:w-full sm:w-80 lg:m-20 lg:p-0 md:p-0 sm:p-0 ' />
                             <div>
-                                {/* <h1 className='lg:px-10 text-5xl lg:text-8xl  lg:ml-11 lg:mt-60 font-bold' style={{ paddingLeft: '15px' }}>Live and let live</h1> */}
                                 <h1 className='lg:px-10 text-5xl lg:text-5xl  lg:ml-11 lg:mt-80 font-bold' style={{ paddingLeft: '15px' }}>Grow more and Feel plants</h1>
                                 <h4 className='lg:px-48 text-xl  text-amber-800 lg:ml-12 font-regular mt-5' style={{ paddingLeft: '15px' }}>It is time to switch to the green revolution.Green would be your new favorite color soon.</h4>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 {/* <!-- Inner --> */}
 
@@ -79,8 +139,11 @@ const Banner = () => {
                     data-bs-target="#carouselDarkVariant"
                     data-bs-slide="prev"
                 >
-                    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
+
+                    <span className=" bg-slate-700  rounded-full p-1.5  inline-block bg-no-repeat text-white" aria-hidden="true">
+                        <i class="uil uil-angle-left text-4xl "></i></span>
+
+                    <span className="visually-hidden ">Previous</span>
                 </button>
                 <button
                     className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 invisible md:visible lg:visible xl:visible"
@@ -88,10 +151,11 @@ const Banner = () => {
                     data-bs-target="#carouselDarkVariant"
                     data-bs-slide="next"
                 >
-                    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className=" bg-slate-700  rounded-full p-1.5   inline-block bg-no-repeat" aria-hidden="true"><i class="uil uil-angle-right text-4xl"></i></span>
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
+
         </div >
 
     );
