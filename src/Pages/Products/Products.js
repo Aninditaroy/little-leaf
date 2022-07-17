f9f53e8d821bc117c853adc0ec06445fa62fc191
Sajeda Saju <sajedasaju85@gmail.com>
2022-07-17 20:31:42 +0600

    pagination added

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -5,7 +5,30 @@ import Product from '../Product/Product';
 import './Products.css';
 import useProducts from './../../Hooks/useProducts';
 const Products = () => {
-    const [products] = useProducts()
+    // const [products] = useProducts();
+    const [products, setProducts] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0)
+    const size = 10;
+
+    useEffect(() => {
+        // fetch('https://rocky-anchorage-54101.herokuapp.com/product')
+        fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [page, size])
+
+
+    useEffect(() => {
+        // fetch('https://rocky-anchorage-54101.herokuapp.com/product')
+        fetch('http://localhost:5000/productCount')
+            .then(res => res.json())
+            .then(data => {
+                const count = data.count;
+                const pages = Math.ceil(count / 10);
+                setPageCount(pages)
+            })
+    }, [])
     return (
         <div>
             <div className='w-5/6 mx-auto my-10'>
@@ -20,6 +43,18 @@ const Products = () => {
 
 
                 </div>
+
+                <div class="btn-group mx-auto justify-center">
+                    {/* <button class="btn">1</button>
+                    <button class="btn btn-active">2</button>
+                    <button class="btn">3</button>
+                    <button class="btn">4</button> */}
+                    {
+                        [...Array(pageCount).keys()].map(number => <button onClick={() => setPage(number)} className={page === number ? "btn btn-active" : 'btn '}
+                        >{number + 1}</button>)
+                    }
+                </div>
+
             </div>
         </div>
     );
