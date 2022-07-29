5dfc6f4d9a84b7cf18241c8e0767bd390947a66e
Sajeda Saju <sajedasaju85@gmail.com>
2022-07-30 00:47:57 +0600

    code added

--- src/Pages/Dashboard/AddBlogs.js
+++ src/Pages/Dashboard/AddBlogs.js
@@ -1,75 +1,172 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddBlogs = () => {
+    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
     return (
         <div>
             <div className='' style={{ backgroundImage: 'url(https://htmldemo.net/pronia/pronia/assets/images/breadcrumb/bg/1-1-1919x388.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
                 <h2 className='text-2xl lg:text-4xl py-20 text-center my-orders-font'><span className='font-bold md:font-extrabold lg:font-extrabold'>Add Blogs</span></h2>
             </div>
 
-            <div>
-                <div class=" w-4/6 mx-auto my-12">
+            <div className='bg-gray-50 w-4/6 w- w-3/4  xl:w-1/2 my-16 mx-auto  mt-12 rounded-xl '>
+                <div className='w-3/4 w-4/5 w-11/12 w-full mx-auto py-8'>
+                    {/* <div className='mb-3'>
+                        <h1 className='text-2xl font-bold text-center mt-2 text-[#73AB24]'><i class="uil uil-plus pr-1"></i>Add Product</h1>
+                    </div> */}
+                    <div className='w-full '>
+                        <div className='flex flex-col items-center '>
 
-                    <div class="mt-5 md:mt-0 ">
-                        <form action="#" method="POST">
-                            <div class="sm:rounded-md sm:overflow-hidden shadow-lg">
-                                <div class="px-4 py-5 bg-white space-y-6 sm:p-6 ">
-
-
-                                    <div className=''>
+                            <form className='w-full'>
+                                <div class="flex items-center justify-between mt-4">
+                                    <div class='w-full md:w-1/2 pr-2'>
                                         <div>
-                                            <label class="block text-sm font-medium text-gray-700">
-                                                Author Photo Image
-                                            </label>
-                                            <div class="mt-1 flex items-center">
-                                                <span class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                                                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                                                    </svg>
-                                                </span>
-                                                <input type="file" class="ml-1 bg-white py-2 px-2 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" />
-                                            </div>
+                                            <label class='block  tracking-wide text-gray-700 text-xs font-bold mb-2' >Author Name</label>
+                                            <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text' {...register("authorName", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Author name is required"
+                                                }
+                                            })} />
                                         </div>
-
+                                        {/* <label className="label">
+                                            {errors.plantName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.plantName.message}</span>}
+                                        </label> */}
                                     </div>
 
+                                    <div class='w-full md:w-1/2 input-bordered'>
+                                        <label class='block  tracking-wide text-gray-700 text-xs font-bold mb-2' > Categories</label>
 
 
-                                    <div>
-                                        <label class="block text-sm font-medium text-gray-700">
-                                            Cover photo
+                                        {/* <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text' required /> */}
+                                        <select class=' block  text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 app leading-tight input-bordered focus:outline-none  focus:border-gray-500 select w-full max-w-xs' {...register("categories", {
+                                            required: {
+                                                value: true,
+                                                message: "Categories is required"
+                                            }
+                                        })}>
+                                            <option value="Indoor plants">Indoor plants</option>
+                                            <option value="Office plants">Office plants</option>
+                                            <option value="Outdoor plants">Outdoor plants</option>
+                                        </select>
+                                        {/* <label className="label">
+                                            {errors.categories?.type === 'required' && <span className="label-text-alt text-red-500">{errors.categories.message}</span>}
+                                        </label> */}
+                                    </div>
+                                </div>
+                                <div class="flex items-center justify-between">
+                                    <label className="label">
+                                        {errors.plantName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.plantName.message}</span>}
+                                    </label>
+                                    <label className="label">
+                                        {errors.categories?.type === 'required' && <span className="label-text-alt text-red-500">{errors.categories.message}</span>}
+                                    </label>
+                                </div>
+                                <div className="form-control w-full ">
+                                    <label className="label">
+                                        <span className="block  tracking-wide text-gray-700 text-xs font-bold mb-2">Description</span>
+                                    </label>
+                                    <textarea
+                                        type="text"
+                                        placeholder="Plant description"
+                                        className="input input-bordered h-32 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                                        {...register("description", {
+                                            required: {
+                                                value: true,
+                                                message: "Description is required"
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.description?.type === 'required' && <span className="label-text-alt text-red-500">{errors.description.message}</span>}
+                                    </label>
+                                </div>
+                                <div class="flex items-center justify-between mt-4">
+                                    <div class='w-full md:w-1/2  mb-6 pr-2'>
+                                        <label class='block  tracking-wide text-gray-700 text-xs font-bold mb-2' >Price</label>
+                                        <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='number'
+                                            {...register("price",
+                                                {
+                                                    required: {
+                                                        value: true,
+                                                        message: "Price is required"
+                                                    },
+                                                    min: {
+                                                        value: 1,
+                                                        message: `Price must be greater than 0`
+                                                    }
+                                                }
+
+                                            )} />
+                                        <label className="label">
+                                            {errors.price?.type === 'required' && <span className="label-text-alt text-red-500">{errors.price.message}</span>}
+                                            {errors.price?.type === 'min' && <span className="label-text-alt text-red-500">{errors.price.message}</span>}
                                         </label>
-                                        <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                                            <div class="space-y-1 text-center">
-                                                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="True">
-                                                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                                </svg>
-                                                <div class="flex text-sm text-gray-600">
-                                                    <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                                        <span>Upload a file</span>
-                                                        <input id="file-upload" name="file-upload" type="file" class="sr-only" />
-                                                    </label>
-                                                    <p class="pl-1">or drag and drop</p>
-                                                </div>
-                                                <p class="text-xs text-gray-500">
-                                                    PNG, JPG, GIF up to 10MB
-                                                </p>
-                                            </div>
-                                        </div>
+                                    </div>
+                                    <div class='w-full md:w-1/2 mb-6'>
+                                        <label class='block  tracking-wide text-gray-700 text-xs font-bold mb-2' >In Stock</label>
+                                        <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='number' {...register("inStock", {
+                                            required: {
+                                                value: true,
+                                                message: "Instock is required"
+                                            }
+                                            ,
+                                            min: {
+                                                value: 0,
+                                                message: `Stock quantity cannot be negative`
+                                            }
+                                        })} />
+                                        <label className="label">
+                                            {errors.inStock?.type === 'required' && <span className="label-text-alt text-red-500">{errors.inStock.message}</span>}
+                                            {errors.inStock?.type === 'min' && <span className="label-text-alt text-red-500">{errors.inStock.message}</span>}
+                                        </label>
                                     </div>
                                 </div>
-                                <div class="px-4 py-3  text-right sm:px-6">
-                                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        Save
-                                    </button>
+                                <div class="w-full mb-5">
+                                    <label class="block flex flex-col items-center px-4 py-2 border-2 border-dashed text-blue rounded-lg shadow-lg bg-gray-200 border-black/30 tracking-wide uppercase border-blue cursor-pointer">
+                                        <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                                        </svg>
+                                        <span class="mt-2 block  tracking-wide text-gray-700 text-xs font-bold mb-2">Select a plant image</span>
+                                        <input type='file' class="hidden"
+                                            {...register("image", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Please select an image"
+                                                }
+                                            })} />
+                                        <label className="label">
+                                            {errors.image?.type === 'required' && <span className="label-text-alt text-red-500">{errors.image.message}</span>}
+                                        </label>
+                                    </label>
+                                    {/* <input type="file" name="chooseFile" id="chooseFile"></input> */}
                                 </div>
-                            </div>
-                        </form>
+                                <div className="form-control w-full ">
+                                    <label className="label">
+                                        <span className="block  tracking-wide text-gray-700 text-xs font-bold mb-2">Image Alt</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Plant Image Alt"
+                                        className="input input-bordered appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                                        {...register("imageAlt", {
+                                            required: {
+                                                value: true,
+                                                message: "Image alt is required"
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.imageAlt?.type === 'required' && <span className="label-text-alt text-red-500">{errors.imageAlt.message}</span>}
+                                    </label>
+                                </div>
+                                <input className='btn w-full flex justify-center items-center mx-auto max-w-xs text-white hover:bg-[#73ab24be]  bg-[#73AB24]  hover:ease-in-out shadow-2xl' type="submit"
+                                    value='Save Changes' />
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
     );
 };
